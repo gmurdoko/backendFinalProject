ec93e9a372054198156c30c7a6c28109e38ac6cd
Dinda Maharani <34880692+DindaMhn@users.noreply.github.com>
2020-09-01 10:14:27 +0700

    dinda first commit

--- main/master/initData.go
+++ main/master/initData.go
@@ -9,18 +9,18 @@ import (
 // Init app
 func Init(r *mux.Router, db *sql.DB) {
 	//Rooms
-	roomRepo := repositories.InitRoomRepoImpl(db)
-	roomUsecase := usecases.InitRoomUsecaseImpl(roomRepo)
-	controllers.RoomController(r, roomUsecase)
+	// roomRepo := repositories.InitRoomRepoImpl(db)
+	// roomUsecase := usecases.InitRoomUsecaseImpl(roomRepo)
+	// controllers.RoomController(r, roomUsecase)
 
-	//Transaction
-	reserveRepo := repositories.InitReserveRepoImpl(db)
-	reserveUsecase := usecases.InitReserveUsecaseImpl(reserveRepo)
-	controllers.ReserveController(r, reserveUsecase)
+	// //Transaction
+	// reserveRepo := repositories.InitReserveRepoImpl(db)
+	// reserveUsecase := usecases.InitReserveUsecaseImpl(reserveRepo)
+	// controllers.ReserveController(r, reserveUsecase)
 
-	//Auth
-	userRepo := repositories.InitUserRepoImpl(db)
-	userUsecase := usecases.InitUserUsecaseImpl(userRepo)
-	controllers.UserController(r, userUsecase)
-	r.Use(logger.ActivityLogMiddleware)
+	// //Auth
+	// userRepo := repositories.InitUserRepoImpl(db)
+	// userUsecase := usecases.InitUserUsecaseImpl(userRepo)
+	// controllers.UserController(r, userUsecase)
+	// r.Use(logger.ActivityLogMiddleware)
 }
