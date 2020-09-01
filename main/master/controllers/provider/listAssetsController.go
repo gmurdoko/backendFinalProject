f5953b2ab8f0790f786422b6dcf22ca631fb8496
juanaf31 <50581538+juanaf31@users.noreply.github.com>
2020-09-01 22:16:54 +0700

    change response

--- main/master/controllers/provider/listAssetsController.go
+++ main/master/controllers/provider/listAssetsController.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"finalproject/main/master/models"
 	providerlistassetsusecase "finalproject/main/master/usecases/provider/listassets"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -24,14 +23,14 @@ func (s *ListAssetsHandler) getListAssets(w http.ResponseWriter, r *http.Request
 	params := mux.Vars(r)
 	userId := params["id"]
 	listAssets, err := s.listAssetsUsecase.GetAll(userId)
-	if err != nil {
-		log.Println(err)
-		w.Write([]byte("Data Not Found!"))
-	}
 	var response models.Response
 	response.Status = http.StatusOK
 	response.Message = "Success"
-	response.Response = listAssets
+	if err != nil || listAssets == nil {
+		response.Response = "Data Not Found"
+	} else {
+		response.Response = listAssets
+	}
 	byteData, err := json.Marshal(response)
 	if err != nil {
 		w.Write([]byte("Something Wrong on Marshalling Data"))
