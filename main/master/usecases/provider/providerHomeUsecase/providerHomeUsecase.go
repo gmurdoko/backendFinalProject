5dc14f9247f902f872f12ac992cf4b3230eb81bf
Dinda Maharani <34880692+DindaMhn@users.noreply.github.com>
2020-09-02 22:37:36 +0700

    [UPDATE] create asset done

--- main/master/usecases/provider/providerHomeUsecase/providerHomeUsecase.go
+++ main/master/usecases/provider/providerHomeUsecase/providerHomeUsecase.go
@@ -1,8 +1,11 @@
 package providerHomeUsecase
 
-import "finalproject/main/master/models"
+import (
+	"finalproject/main/master/models"
+	"mime/multipart"
+)
 
 type ProviderHome interface {
 	GetProviderSaldo(string) (int, error)
-	CreateAssetProvider(*models.AssetModel) (*models.AssetModel, error)
+	CreateAssetProvider(photo multipart.File, handler *multipart.FileHeader, provider *models.AssetModel) (*models.AssetModel, error)
 }
