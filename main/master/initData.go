5a856298a06e9974e1747d881f730c35b4383655
juanaf31 <50581538+juanaf31@users.noreply.github.com>
2020-09-02 11:28:41 +0700

    add init data

--- main/master/initData.go
+++ main/master/initData.go
@@ -2,8 +2,17 @@ package master
 
 import (
 	"database/sql"
-	controllers "finalproject/main/master/controllers/provider"
+	controllersAdmin "finalproject/main/master/controllers/admin"
+	controllersProvider "finalproject/main/master/controllers/provider"
+
+	adminaccountmanagementrepo "finalproject/main/master/repositories/admin/accountManagement"
+	adminassetsreportsrepo "finalproject/main/master/repositories/admin/report"
+	providerassetsreportsrepo "finalproject/main/master/repositories/provider/assetReport"
 	providerlistassetsrepo "finalproject/main/master/repositories/provider/listAssets"
+
+	adminaccountmanagementusecase "finalproject/main/master/usecases/admin/accountManagement"
+	adminassetsreportsusecase "finalproject/main/master/usecases/admin/report"
+	providerassetreportsusecase "finalproject/main/master/usecases/provider/assetReport"
 	providerlistassetsusecase "finalproject/main/master/usecases/provider/listassets"
 
 	"github.com/gorilla/mux"
@@ -13,5 +22,17 @@ import (
 func Init(r *mux.Router, db *sql.DB) {
 	listAssetsRepo := providerlistassetsrepo.InitListAssetsRepoImpl(db)
 	listAssetsUsecase := providerlistassetsusecase.InitListAssetsUsecaseImpl(listAssetsRepo)
-	controllers.ListAssetsController(r, listAssetsUsecase)
+	controllersProvider.ListAssetsController(r, listAssetsUsecase)
+
+	providerAssetsReportRepo := providerassetsreportsrepo.InitProviderAssetReportRepoImpl(db)
+	providerAssetsReportUsecase := providerassetreportsusecase.InitProviderReportUsecaseImpl(providerAssetsReportRepo)
+	controllersProvider.ProviderAssetReportController(r, providerAssetsReportUsecase)
+
+	adminAssetsReportRepo := adminassetsreportsrepo.InitAdminAssetReportRepoImpl(db)
+	adminAssetsReportUsecase := adminassetsreportsusecase.InitProviderReportUsecaseImpl(adminAssetsReportRepo)
+	controllersAdmin.AdminAssetReportController(r, adminAssetsReportUsecase)
+
+	adminAccountManagementRepo := adminaccountmanagementrepo.InitAccountManagementImpl(db)
+	adminAccountManagementUsecase := adminaccountmanagementusecase.InitAccountManagementUsecaseImpl(adminAccountManagementRepo)
+	controllersAdmin.AccountManagerController(r, adminAccountManagementUsecase)
 }
