9b18e29269083dbc008faad2c9ace2b1426e89a7
Dinda Maharani <34880692+DindaMhn@users.noreply.github.com>
2020-09-06 22:56:59 +0700

    [UPDATE] add getbyid

--- main/master/controllers/user/accountController.go
+++ main/master/controllers/user/accountController.go
@@ -21,7 +21,7 @@ func UserAccController(r *mux.Router, service userAccountUsecase.UserAccount) {
 	auth := r.PathPrefix("/authUser").Subrouter()
 	auth.HandleFunc("/login", userHandler.GetUser).Methods(http.MethodPost)
 	auth.HandleFunc("/register", userHandler.CreateUsers).Methods(http.MethodPost)
-
+	auth.HandleFunc("/{id}", userHandler.GetUserById).Methods(http.MethodGet)
 }
 func (uh *UserAccHandler) GetUser(w http.ResponseWriter, r *http.Request) {
 	var data models.UserModel
@@ -78,6 +78,28 @@ func (uh *UserAccHandler) CreateUsers(w http.ResponseWriter, r *http.Request) {
 		response.Status = http.StatusOK
 		response.Message = "Success"
 		response.Data = data
+		byteData, err := json.Marshal(response)
+		if err != nil {
+			w.Write([]byte("Something Wrong on Marshalling Data"))
+		}
+		w.Header().Set("Content-type", "application/json")
+		w.Write(byteData)
+	}
+}
+func (uh *UserAccHandler) GetUserById(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id := params["id"]
+	user, _ := uh.userUsecase.GetUserById(id)
+	// var response models.Response
+	token, err := jwt.JwtEncoder(user.Username, "rahasiadong")
+	if err != nil {
+		http.Error(w, "Failed token generation", http.StatusUnauthorized)
+	} else {
+		var response response.Response
+		response.Status = http.StatusOK
+		response.Message = "Success"
+		response.Token = token
+		response.Data = user
 		byteData, err := json.Marshal(response)
 		if err != nil {
 			w.Write([]byte("Something Wrong on Marshalling Data"))
